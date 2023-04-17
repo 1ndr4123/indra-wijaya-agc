3dd5f3fb69efb9a4d1b3312cc0b49a00d31bbc1e
1ndr4123 <99191064+1ndr4123@users.noreply.github.com>
2023-04-17 14:05:31 +0700

    Switch, Ramp, Launcher"
    tugas 3 - Implementasi Switch, Ramp, Launcher

--- indra wijaya - game pin ball/Assets/Scripts/BumperController.cs
+++ indra wijaya - game pin ball/Assets/Scripts/BumperController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class BumperController : MonoBehaviour
 {
-    public Collider bola;
-    public float multiplier = 2f; // deklarasi variabel multiplier dan menginisialisasi nilainya
+  public Collider bola;
+  public float multiplier;
+  public Color color;
 
-    private void OnCollisionEnter(Collision collision)
+  private Renderer renderer;
+  private Animator animator;
+
+  private void Start()
+  {
+    renderer = GetComponent<Renderer>();
+    animator = GetComponent<Animator>();
+
+    renderer.material.color = color;
+  }
+
+  private void OnCollisionEnter(Collision collision)
+  {
+    if (collision.collider == bola)
     {
-        if (collision.collider == bola)
-        {
-            Rigidbody bolaRig = bola.GetComponent<Rigidbody>();
-            bolaRig.velocity *= multiplier; // menggunakan variabel multiplier yang sudah dideklarasikan dan diinisialisasi
-        }
+      Rigidbody bolaRig = bola.GetComponent<Rigidbody>();
+      bolaRig.velocity *= multiplier;
+
+      animator.SetTrigger("hit");
     }
+  }
 }
